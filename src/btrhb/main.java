a7537ece7187466248ca3f5cd2b12a2bd16a13f7
Thu Tran <chiujde6737@gmail.com>
2023-12-24 23:38:57 +0700

    da co su thay doi

--- src/btrhb/main.java
+++ src/btrhb/main.java
@@ -1,7 +1,8 @@
 package btrhb;
 
 public class main {
-public static void main(String[] args) {
-	System.out.print("thu");
-}
+	public static void main(String[] args) {
+		System.out.print("thu");
+		System.out.print(" xuat sac");
+	}
 }
